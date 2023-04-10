2e28b9358bcd3612c9c6e167aa71bdceabcbfb66
JuanCarlo Castillo <juancarlo.castillo20@gmail.com>
2023-04-11 01:17:22 +0200

    First Part

---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Search(props) {
+    return (
+        <div className="Search">
+                <input type='text' />
+
+        </div>
+    );
+}
+
+export default Search;
